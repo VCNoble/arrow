186b13b242ed1e084f045f697b82646f161c540e
VienCN <noble.vien@gmail.com>
2022-10-08 13:58:28 +1300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Services from './components/Services';
+import Subscribe from './components/Subscribe';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Services/>
+      <Subscribe/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Services.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Box from './Box';
+import image1 from '../images/s1.png';
+import image2 from '../images/s2.png';
+
+function Services() {
+  return (
+    <div id='services'>
+        <div className="s-heading">
+        <h1>Services</h1>
+        <p>Here are some Services I provide</p>
+      </div>
+      <div className="b-container">
+        <Box image={image1} alte='image1' button='Python'/>
+        <Box image={image2} alte='image1' button='React'/>
+        <Box image={image1} alte='image1' button='HTML'/>
+
+        </div>
+    </div>
+  )
+}
+
+export default Services;
